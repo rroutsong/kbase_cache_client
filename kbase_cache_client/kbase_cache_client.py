5ca70d50ef41d2edcaf65fd4663723c51c35784c
Ryan Routsong <rroutson@utk.edu>
2019-03-20 15:18:49 -0400

    finished with client tests

--- kbase_cache_client/kbase_cache_client.py
+++ kbase_cache_client/kbase_cache_client.py
@@ -34,22 +34,10 @@ class UnknownRequestError(Exception):
 class DownloadDirNotaDir(Exception):
     pass
 
-class DownloadDirNotWritable(Exception):
+class DownloadDirNotWriteable(Exception):
     pass
 
 class KBaseCacheClient:
-    def __init__(self, service, cache_id=""):
-        self.callback = service
-        if not self.callback.endswith('/'):
-            self.cacheurl = self.callback + '/cache/v1'
-        else:
-            self.cacheurl = self.callback + 'cache/v1'
-        if config.get('KBASE_CACHE_SERVICE', 'TOKEN', fallback=None):
-            self.service_token = config.get('KBASE_CACHE_SERVICE', 'TOKEN', fallback=None)
-        elif os.getenv('KBASE_CACHE_TOKEN', None):
-            self.service_token = os.getenv('KBASE_CACHE_TOKEN', None)
-        self.cache_id = cache_id
-
     def generate_cacheid(self, identifiers):
         if not isinstance(identifiers, dict):
             raise NoCacheIdentifiers('Identifiers for cache id must be in dictionary format.')
@@ -61,27 +49,52 @@ class KBaseCacheClient:
         else:
             endpoint = self.callback + 'cache/v1/cache_id'
 
-        req_call = requests.post(endpoint, data=json.dumps(identifiers), headers=headers)
-        pp(req_call)
-        exit(endpoint)
+        req_call = requests.post(endpoint, data=json.dumps(identifiers), headers=headers).json()
 
         if req_call.get('error'):
             pp(req_call)
             raise HTTPRequestError(req_call.json().get('error'))
         else:
             self.cache_id = req_call['cache_id']
-            return req_call['cache_id']
+            return self.cache_id
+
+    def __init__(self, service, token=None, cache_id=None, identifiers=None):
+        self.callback = service
+        if not self.callback.endswith('/'):
+            self.cacheurl = self.callback + '/cache/v1/'
+        else:
+            self.cacheurl = self.callback + 'cache/v1/'
+        if token is None:
+            if config.get('KBASE_CACHE_SERVICE', 'TOKEN', fallback=None):
+                self.service_token = config.get('KBASE_CACHE_SERVICE', 'TOKEN', fallback=None)
+            elif os.getenv('KBASE_CACHE_TOKEN', None):
+                self.service_token = os.getenv('KBASE_CACHE_TOKEN', None)
+        else:
+            self.service_token = token
+        # TODO: make note in docs that each instance of Cache Client should be used
+        #   for each cache.
+        if cache_id is None:
+            if identifiers is not None:
+                self.cache_id = self.generate_cacheid(identifiers)
+            else:
+                self.cache_id = None
+        else:
+            self.cache_id = cache_id
+
+    def get_cache_id(self):
+        if self.cache_id is not None:
+            return self.cache_id
+        else:
+            return False
+
+    def set_cache_id(self, new_cache_id):
+        self.cache_id = new_cache_id
+        return True
 
     def download_cache(self, destination):
         headers = {'Content-type': 'application/json', 'Authorization': self.service_token}
-        endpoint = self.cacheurl + self.cache_id
+        endpoint = self.cacheurl + 'cache/' + self.cache_id
         req_call = requests.get(endpoint, headers=headers, stream=True)
-
-        if not os.path.isdir(destination):
-            raise DownloadDirNotaDir('Please pass a writeable directory path to download_cache')
-        else:
-            if not os.access(destination, os.W_OK):
-                raise DownloadDirNotWriteable('Please pass a writeable directory path to download_cache')
 
         if req_call.status_code == 200:
             print(f'Downloading cache {self.cache_id}...\nTo: {destination}')
@@ -101,7 +114,8 @@ class KBaseCacheClient:
 
     def upload_cache(self, source):
         headers = {'Authorization': self.service_token}
-        endpoint = self.cacheurl + self.cache_id
+        endpoint = self.cacheurl + 'cache/' + self.cache_id
+
         with open(source, 'rb') as f:
             req_call = requests.post(endpoint, files={'file': f}, headers=headers)
 
@@ -109,16 +123,18 @@ class KBaseCacheClient:
             print('Cache ' + self.cache_id + ' has been successfully uploaded')
             return True
         elif req_call.json().get('error'):
-            pp(req_call.json().get('error'))
+            pp(req_call.json())
+            pp(endpoint)
             raise HTTPRequestError('An error with the request occurred see above error message.')
         else:
             pp(req_call)
+            pp(endpoint)
             raise UnknownRequestError(f'Request status code: {req_call.status_code}\n'
                              f'Unable to complete request action')
 
     def delete_cache(self):
         headers = {'Authorization': self.service_token}
-        endpoint = self.cacheurl + self.cache_id
+        endpoint = self.cacheurl + 'cache/' + self.cache_id
         req_call = requests.delete(endpoint, headers=headers)
 
         if req_call.status_code == 200:
--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ import shutil
 class TestKbaseCacheClient(unittest.TestCase):
     @classmethod
     def setUp(cls):
-        cls.KBC = KBaseCacheClient('https://ci.kbase.us/services/')
+        cls.KBC = KBaseCacheClient('https://appdev.kbase.us/services/')
         cls.test_dir = os.path.join(os.path.dirname(__file__), 'tests')
         if not os.path.exists(cls.test_dir):
             os.mkdir(cls.test_dir)
@@ -16,7 +16,7 @@ class TestKbaseCacheClient(unittest.TestCase):
             text_file = open(cls.test_file, "w")
             text_file.write('This is the file to test the cache client module')
             text_file.close()
-
+            
     @classmethod
     def tearDownClass(cls):
         shutil.rmtree(cls.test_dir)
@@ -24,21 +24,15 @@ class TestKbaseCacheClient(unittest.TestCase):
     def test_gen_cache_id(self):
         self.cacheid = self.KBC.generate_cacheid({'test': 'this is a test identifier to id a cache file'})
         print(f'Cache ID is {self.cacheid}')
-    """
-
-    def test_upload_cache(self):
-        source = self.test_file
-        self.KBC.upload_cache(source)
-
-    def test_download_cache(self):
-        destination = os.path.join(self.test_dir, 'cache')
-        os.mkdir(destination)
-
+        self.KBC.upload_cache(self.test_file)
+        print('Cache uploaded.')
+        destinationdir = os.path.join(self.test_dir, 'cache')
+        os.mkdir(destinationdir)
+        destination = os.path.join(destinationdir, 'cachedl.txt')
         self.KBC.download_cache(destination)
-
-    def test_delete_cache(self):
+        print('Cache downloaded.')
         self.KBC.delete_cache()
-    """
+        print('Cache deleted.')
 
 if __name__ == '__main__':
     unittest.main()
