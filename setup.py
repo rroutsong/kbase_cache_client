246cb04ba5e807fa339f4e955455fb0a1defb0d1
Jay R Bolton <jayrbolton@gmail.com>
2019-03-26 16:20:50 -0700

    0.0.1->0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='kbase_cache_client',
     description='Python client for the KBase file cache server',
-    version='0.0.1',
+    version='0.0.2',
     author='rroutsong',
     author_email='info@kbase.us',
     url='https://github.com/rroutsong/kbase_cache_client',
